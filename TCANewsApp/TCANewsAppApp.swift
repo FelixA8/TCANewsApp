0c7095a538eebfeb3d32d86dee6ca9f8fdbdcf74
Felix Anderson <felix.anderson@gdn-commerce.com>
2025-03-01 22:05:44 +0700

    Initial Commit

---
+++ TCANewsApp/TCANewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCANewsAppApp.swift
+//  TCANewsApp
+//
+//  Created by Felix Anderson on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TCANewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
